0e933765a3d25d287c1bdf37a2140ea7581a4600
Ritik Garg <ritikgarg.1773@gmail.com>
2018-10-06 03:52:08 +0530

    Testing

---
+++ score_implementation.js
@@ -0,0 +1,18 @@
+score1 = 100
+score2 = 100
+
+if (fire1 == 1)
+	if (p2y >= p1y-75 && p2y < p1y + 75 )
+		score2 = score2- 15
+	
+if (fire2 == 1)
+	if (p1y >= p2y && p1y < p2y + 50 )
+		score1 = score1- 15
+
+if (fart1 == 1)
+	if (p2y >= p1y-75 && p2y < p1y + 75 )
+		score2 = score2- 15
+	
+if (fart2 == 1)
+	if (p1y >= p2y && p1y < p2y + 50 )
+		score1 = score1- 15
